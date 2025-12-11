e67cb657e0b037a206f9ee4ef2bcf9e45f4c0857
Moonie-psudo <monira.2work@gmail.com>
2025-12-11 22:05:14 +0600

    4th

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ run().catch(console.dir);
 
 // ================= Basic Routes =================
 app.get('/', (req, res) => res.send('HELLO WORLD!'));
-app.get('/home', (req, res) => res.send('This am home'));
+app.get('/home', (req, res) => res.send('This i home'));
 
 // Start server
 app.listen(port, () => {
